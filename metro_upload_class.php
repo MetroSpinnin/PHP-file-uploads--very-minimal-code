6fa8313baf75b7fd574f0708669f86172667b0cb
Samuel Jim Nnamdi <46245794+MetroSpinnin@users.noreply.github.com>
2019-02-28 10:10:58 +0100

    Update metro_upload_class to add comments

--- metro_upload_class.php
+++ metro_upload_class.php
@@ -17,5 +17,33 @@ class MetroFileUploads{
 
 $target_directory_to_push_image = "C:/users/temp/data";
 
-function 
+function moveUploadedFileToDestination(){
+  /*
+  * We would create a variable and initialize
+  * it to hold our conditions which would aid
+  * to push our pictures to the right folder
+  */
+  
+  if(is_array($_FILES)){
+    
+    /*
+    * The @param (is_uploaded_file) is an inbuilt
+    * PHP function which is used to check the file 
+    * and also holds the temporary name of the pic
+    */
+    
+    if(is_uploaded_file($_FILES["myPicture"]["tmp_name"]){
+      
+      /*
+      * The @param (move_uploaded_file) is an inbuilt PHP
+      * function which moves the picture from a temporary 
+      * folder to the folder it is meant to stay
+      */
+      
+      if(move_uploaded_file($FILES["myPicture"]["tmp_name"],"$target_directory_to_push_image". $_FILES["myPicture"]["name"]){
+        echo "file uploaded successfully";
+      }
+    }
+  }
+}
 }
