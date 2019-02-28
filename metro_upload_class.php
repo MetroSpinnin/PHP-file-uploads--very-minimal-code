dc7a387fa301180ea95825c4ac09528748be3bd0
Samuel Jim Nnamdi <46245794+MetroSpinnin@users.noreply.github.com>
2019-02-28 09:59:19 +0100

    create the base class to hold the file upload to call

---
+++ metro_upload_class.php
@@ -0,0 +1,21 @@
+<?php 
+
+/*
+* Firstly note that we could have done this basically
+* done ths the procedural way, but to get things done
+* according to best practices that is why am trying to
+* Use the object oriented pattern of things
+*/
+
+class MetroFileUploads{
+
+/*
+* The code below simply states the particular
+* directory which we want to put or rather store
+* the images.
+*/
+
+$target_directory_to_push_image = "C:/users/temp/data";
+
+function 
+}
