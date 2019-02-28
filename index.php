ed1c96ddb8bd422a3db1fe8f42f6b6dc582ad0b2
Samuel Jim Nnamdi <46245794+MetroSpinnin@users.noreply.github.com>
2019-02-28 10:17:46 +0100

    update index.php and use func

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
+<?php
 
+/*
+* Require the class created in the
+* metro_upload_class.php
+*/
+
+require_once("metro_upload_class.php");
+
+/*
+* This code serves the html form which
+* would be triggered to certainly use 
+* the function created in the metro_
+* upload_class.php to aid uploads
+*/
+
+echo "<form name = 'frm_add_picture' method = 'post'>";
+echo "<input type = 'file' name = 'myPicture'>";
+echo "</form>";
+
+?>
